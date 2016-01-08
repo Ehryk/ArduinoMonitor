b6ca121eaa80cc402bc69e87287ec0412d8f3f0c
Ehryk <rhaistlin@gmail.com>
2016-01-08 02:30:17 -0600

    ArduinoWeb: Renamed Index to Dashboard

--- ArduinoMonitor/ArduinoWeb/Controllers/HomeController.cs
+++ ArduinoMonitor/ArduinoWeb/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace ArduinoWeb.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Dashboard()
         {
             ViewBag.Title = "Dashboard";
 

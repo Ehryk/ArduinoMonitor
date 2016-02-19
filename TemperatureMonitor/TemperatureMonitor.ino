e000e713a85277ec61941149961a73f86518f669
Ehryk <rhaistlin@gmail.com>
2016-02-18 23:46:26 -0600

    TemperatureMonitor: version bump to 0.3: using float, updating pins, using faster serial communication

--- TemperatureMonitor/TemperatureMonitor.ino
+++ TemperatureMonitor/TemperatureMonitor.ino
@@ -1,50 +1,52 @@
 #include "DHT.h"
 
-int sampleRate = 2500; //Milliseconds between sampling
-bool debug = false;
-float tCorrection = -14.0;
-
+// Pin Definitions
 #define DHTPWR 7 //Power
-#define DHTGND 4 //Ground
 #define DHTPIN 6 //Data Pin (digital)
-
+#define DHTGND 5 //Ground
 #define LM34PWR A0 //Power
 #define LM34GND A2 //Ground
 #define LM34PIN A1 //Data Pin (analog)
-
 #define LIGHTPIN A4 //Data Pin (analog)
 
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // MDHT 11 
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+// Variables
+int sampleRate = 2500; //Milliseconds between sampling
+bool debug = false;
+float internalCorrection = +8.7;
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11 
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   pinMode(DHTPWR, OUTPUT);
   pinMode(DHTGND, OUTPUT);
+  pinMode(DHTPIN, INPUT);
+  
   pinMode(LM34PWR, OUTPUT);
   pinMode(LM34GND, OUTPUT);
   pinMode(LM34PIN, INPUT);
+  
   pinMode(LIGHTPIN, INPUT);
   
-  Serial.begin(9600); 
+  Serial.begin(57600); 
   
   Serial.println("TemperatureMonitor");
-  Serial.println("v0.2 Eric Menze 1.25.2014");
+  Serial.println("v0.3 Eric Menze 2.11.2016");
   Serial.println("Powering Sensors...");
+  
   digitalWrite(DHTPWR, HIGH);
   digitalWrite(DHTGND, LOW);
+  
   digitalWrite(LM34PWR, HIGH);
   digitalWrite(LM34GND, LOW);
+  
   delay(2000);
   dht.begin();
+  
   Serial.println("Beginning Read Loop...");
 }
 
@@ -61,8 +63,7 @@ void loop() {
   float tF = tC * 9 / 5 + 32;
   float l = (1024 - light) / 1024 * 100; //Light Sensor, future use
   
-  //float tArduino = readTemp()/1000.0 + tCorrection;
-  float tArduino = GetTemp() + tCorrection;
+  float tArduino = readInternalTemp();
   float vArduino = readVcc()/1000.0;
 
   // check if returns are valid, if they are NaN (not a number) then something went wrong!
@@ -137,10 +138,10 @@ long readVcc() {
   return result;
 }
 
-double GetTemp(void)
+float readInternalTemp()
 {
   unsigned int wADC;
-  double t;
+  float t;
 
   // The internal temperature has to be used
   // with the internal reference of 1.1V.
@@ -165,5 +166,6 @@ double GetTemp(void)
   t = (wADC - 324.31 ) / 1.22;
 
   // The returned temperature is in degrees Celcius.
-  return (t);
+  return t + internalCorrection;
 }
+
